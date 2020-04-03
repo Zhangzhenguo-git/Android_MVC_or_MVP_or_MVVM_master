ab78a7fd1f7579cd3a9dc6a3dfae2ec02c652c5e
kyzgxiaoguo <826189539@qq.com>
2020-04-03 16:20:53 +0800

    1111

---
+++ mvvm/src/main/java/com/example/mvvm/model/User.java
@@ -0,0 +1,28 @@
+package com.example.mvvm.model;
+
+/**
+ * @author Zhangzhenguo
+ * @create 2020/1/21
+ * @Email 18311371235@163.com
+ * @Describe
+ */
+public class User {
+    private String name;
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
